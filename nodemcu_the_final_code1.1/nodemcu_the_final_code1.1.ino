1cbbf175aa11070b83b6bdfd4375d8620ceaa726
Mostafizur Rahman <59665707+imostafizur@users.noreply.github.com>
2021-04-14 01:44:01 +0600

    Update nodemcu_the_final_code1.1.ino

--- nodemcu_the_final_code1.1/nodemcu_the_final_code1.1.ino
+++ nodemcu_the_final_code1.1/nodemcu_the_final_code1.1.ino
@@ -46,37 +46,7 @@ String recipientID;
 String subject;
 String body;
 
-/*void InputInfo(){ // function to input recipient's email ID, subject and body of the email through the serial monitor
-  Serial.println("\n");
-  Serial.print("Recipient's Email ID : ");
-  while(Serial.available() == 0);
-  recipientID = Serial.readString(); // reads from the serial monitor
-  recipientID.replace("\n", "");
-  Serial.println(recipientID);
-  
-  Serial.print("Subject              : ");
-  while(Serial.available() == 0);
-  subject = Serial.readString(); // reads from the serial monitor
-  subject.replace("\n", "");
-  Serial.println(subject);
 
-  Serial.print("Body                 : ");
-  while(Serial.available() == 0);
-  body = Serial.readString(); // reads from the serial monitor
-  for(int i = 0; i < body.length(); i++) {
-    Serial.print((char)body.charAt(i));
-    if (i % 65 == 0 && i != 0) {
-      if(body.charAt(i) == ' '){
-        Serial.println();
-        Serial.print("                       ");
-      }
-      else{
-        Serial.print("-\n");
-        Serial.print("                       ");
-      }
-    }
-  }
-}*/
 
 void CustomInfo(){ // function to set custom settings
   //replace the below three lines with your information
@@ -144,20 +114,7 @@ void setup()
     if(!connection_state)  // if not connected to WIFI
         Awaits();          // constantly trying to connect
 
-  /*  Gsender *gsender = Gsender::Instance();
-    
-    InputInfo(); //inputs receiver's gmail id, subject and body of the email
-    //CustomInfo(); // uncomment this line if you want to send the email using the custom information which are already set in the code; and comment the above line.
-    
-    if(gsender->Subject(subject)->Send(recipientID, body)) { // sends the email using a single line function
-        Serial.println("Email sent"); // message confirmation
-      
-          
-
-    } else {
-        Serial.print("Error sending message: ");
-        Serial.println(gsender->getError()); // prints out the exact error if the email wasn't successfully sent
-    }*/
+ 
   pinMode(D0, INPUT);
 
    myservo.attach(D8);
